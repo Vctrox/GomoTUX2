145065939bdec51e04b73b685b04f3c8af613518
Iman Alexandre TOURRES <contact@nami10.fr>
2022-05-07 20:23:07 +0200

    new methods to IA class

--- IA.hpp
+++ IA.hpp
@@ -8,8 +8,11 @@ using namespace std;
 
 class IA {
     private:
-        unordered_map<string, int> hashMap;
+        unordered_map<string, int> hashTable;
 
     public:
         int evaluation(Board & board, bool isMax);
+        int minimaxAlphaBeta(Board & board, int depth, bool isMax, int alpha, int beta, int x, int y);
+        void addToHashTable(Board & board, int eval);
+        bool checkHashTable(Board & board);
 };
