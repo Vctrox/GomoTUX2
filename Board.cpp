9b9d0a398240d0327683156e3133fac2bec95776
Tom Xie <xie.tom7@gmail.com>
2022-05-07 20:37:52 +0200

    refacto + gitignore

--- Board.cpp
+++ Board.cpp
@@ -3,110 +3,121 @@
 
 using namespace std;
 
-Board::Board() {
+Board::Board()
+{
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
-            board[i][j] = emptySymbol;
+            board[i][j] = EMPTY_SYMBOL;
     movesCount = 0;
 }
 
-
-void Board::display() {
-    cout <<"   ";
-	for (int i = 0 ; i < N; i++){
-		string s = i < 10?"  ":" ";
-		cout << i <<s;
-	}
-	cout <<endl;
-	for (int i = 0; i < N; i++){
-		string s = i < 10?"  ":" ";
-		cout << i<<s;
-		for (int j = 0; j < N; j++){
-			cout << board[i][j]<<"  ";
-		}
-		// cout <<i;
-		cout <<endl;
-	}
+void Board::display()
+{
+    cout << "   ";
+    for (int i = 0; i < N; i++)
+    {
+        string s = i < 10 ? "  " : " ";
+        cout << i << s;
+    }
+    cout << endl;
+    for (int i = 0; i < N; i++)
+    {
+        string s = i < 10 ? "  " : " ";
+        cout << i << s;
+        for (int j = 0; j < N; j++)
+        {
+            cout << board[i][j] << "  ";
+        }
+        // cout <<i;
+        cout << endl;
+    }
     /*
-	cout <<"   ";
-	for (int i = 0 ; i < N; i++){
-		string s = i < 10?"  ":" ";
-		cout << i <<s;
-	} */
-	cout <<endl;
+    cout <<"   ";
+    for (int i = 0 ; i < N; i++){
+        string s = i < 10?"  ":" ";
+        cout << i <<s;
+    } */
+    cout << endl;
 }
 
-
-bool Board::checkEmpty(int x, int y) {
-    if (x >= 0 && x < N && y >= 0 && y < N && board[x][y] == emptySymbol)
+bool Board::checkEmpty(int x, int y)
+{
+    if (x >= 0 && x < N && y >= 0 && y < N && board[x][y] == EMPTY_SYMBOL)
         return true;
     return false;
 }
 
-
-void Board::setValue(int x, int y, char c) {
+void Board::setValue(int x, int y, char c)
+{
     board[x][y] = c;
-    if (c == emptySymbol)
+    if (c == EMPTY_SYMBOL)
         movesCount--;
     else
         movesCount++;
 }
 
-
-bool Board::rowWinMove(int x, int y) {
+bool Board::rowWinMove(int x, int y)
+{
     int count = 1;
-    for (int i = 1; count < 5 && i < 5 && y+i < N && board[x][y+i] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && y + i < N && board[x][y + i] == board[x][y]; i++)
         count++;
-    for (int i = 1; count < 5 && i < 5 && y-i >= 0 && board[x][y-i] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && y - i >= 0 && board[x][y - i] == board[x][y]; i++)
         count++;
     return count == 5;
 }
 
-
-bool Board::columnWinMove(int x, int y) {
+bool Board::columnWinMove(int x, int y)
+{
     int count = 1;
-    for (int i = 1; count < 5 && i < 5 && x+i < N && board[x+i][y] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && x + i < N && board[x + i][y] == board[x][y]; i++)
         count++;
-    for (int i = 1; count < 5 && i < 5 && x-i >= 0 && board[x-i][y] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && x - i >= 0 && board[x - i][y] == board[x][y]; i++)
         count++;
     return count == 5;
 }
 
-bool Board::firstDiagWinMove(int x, int y) {
+bool Board::firstDiagWinMove(int x, int y)
+{
     int count = 1;
-    for (int i = 1; count < 5 && i < 5 && x+i < N && y+i < N&& board[x+i][y+i] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && x + i < N && y + i < N && board[x + i][y + i] == board[x][y]; i++)
         count++;
-    for (int i = 1; count < 5 && i < 5 && x-i >= 0 && y-i >= 0 && board[x-i][y-i] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && x - i >= 0 && y - i >= 0 && board[x - i][y - i] == board[x][y]; i++)
         count++;
     return count == 5;
 }
 
-bool Board::secondDiagWinMove(int x, int y) {
+bool Board::secondDiagWinMove(int x, int y)
+{
     int count = 1;
-    for (int i = 1; count < 5 && i < 5 && x+i < N && y-i >= 0 && board[x+i][y-i] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && x + i < N && y - i >= 0 && board[x + i][y - i] == board[x][y]; i++)
         count++;
-    for (int i = 1; count < 5 && i < 5 && x-i >= 0 && y+i < N && board[x-i][y+i] == board[x][y]; i++)
+    for (int i = 1; count < 5 && i < 5 && x - i >= 0 && y + i < N && board[x - i][y + i] == board[x][y]; i++)
         count++;
     return count == 5;
 }
 
-bool Board::winMove(int x, int y) {
+bool Board::winMove(int x, int y)
+{
     return rowWinMove(x, y) || columnWinMove(x, y) || firstDiagWinMove(x, y) || secondDiagWinMove(x, y);
 }
 
-bool Board::draw() {
-    return movesCount == N*N;
+bool Board::draw()
+{
+    return movesCount == N * N;
 }
 
-char Board::getValue(int x, int y) {
+char Board::getValue(int x, int y)
+{
     return board[x][y];
 }
 
-int Board::getMovesCount() {
+int Board::getMovesCount()
+{
     return movesCount;
 }
 
-string Board::getBoard() {
+string Board::getBoard()
+{
     string out = "";
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
--- IA.hpp
+++ IA.hpp
@@ -6,13 +6,14 @@
 
 using namespace std;
 
-class IA {
-    private:
-        unordered_map<string, int> hashTable;
+class IA
+{
+private:
+    unordered_map<string, int> hashTable;
 
-    public:
-        int evaluation(Board & board, bool isMax);
-        int minimaxAlphaBeta(Board & board, int depth, bool isMax, int alpha, int beta, int x, int y);
-        void addToHashTable(Board & board, int eval);
-        bool checkHashTable(Board & board);
+public:
+    int evaluation(Board &board, bool isMax);
+    int minimaxAlphaBeta(Board &board, int depth, bool isMax, int alpha, int beta, int x, int y);
+    void addToHashTable(Board &board, int eval);
+    bool checkHashTable(Board &board);
 };
