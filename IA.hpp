abf239efec9af030c8b41e84dfe5657e7e9a80c2
Iman Alexandre TOURRES <contact@nami10.fr>
2022-05-07 20:53:05 +0200

    adding countPattern and constructor to IA class

--- IA.hpp
+++ IA.hpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
-#include <regex>
 
 #include "Board.hpp"
 
 using namespace std;
 
-class IA
-{
-private:
-    unordered_map<string, int> hashTable;
+class IA {
+    private:
+        unordered_map<string, int> hashTable;
 
-public:
-    int evaluation(Board &board, bool isMax);
-
-    int searchPattern(Board &board, string pattern);
-
-    int minimaxAlphaBeta(Board &board, int depth, bool isMax, int alpha, int beta, int x, int y);
-    void addToHashTable(Board &board, int eval);
-    bool checkHashTable(Board &board);
+    public:
+        IA(const string patternFile);
+        int evaluation(Board & board, bool isMax);
+        int countPattern(Board & board, string pattern);
+        int minimaxAlphaBeta(Board & board, int depth, bool isMax, int alpha, int beta, int x, int y);
+        void addToHashTable(Board & board, int eval);
+        bool checkHashTable(Board & board);
 };
