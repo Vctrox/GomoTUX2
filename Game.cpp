68782b53e71eb854e38a4eb6527a57edd60ae766
Tom Xie <xie.tom7@gmail.com>
2022-05-08 20:23:57 +0200

    fix main loop

--- Game.cpp
+++ Game.cpp
@@ -10,16 +10,16 @@ void Game::run()
     //welcome
     cout << "Welcome" << endl;
     cout << "would you like to play? Y/n :";
-    char awnser;
+    char answer;
     
-    while(awnser!='Y' && awnser!='n'){
-        cin >> awnser;
+    while ( answer != 'Y' && answer != 'n'){
+        cin >> answer;
         cout << endl;
-        if(awnser=='n'){
-            cout << "Good by beautie" <<endl;
+        if(answer =='n'){
+            cout << "Good bye beauty" <<endl;
             return;
         }
-        else if(awnser == 'Y'){
+        else if(answer == 'Y'){
             cout << "Welcome to the best game ever made" << endl;
         }
         else{
@@ -31,66 +31,74 @@ void Game::run()
     int role;
     cout << "Would you like to play first or second? 1/2: ";
     
-    while(role!=1 && role!= 2)
+    while(role != 1 && role != 2)
     {
         cin >> role;
         
-        if(role==1 || role==2){
-            cout << "okay you are going to play " << (role==1? "first" : "second") << endl;
+        if(role == 1 || role == 2){
+            cout << "okay you are going to play " << (role == 1 ? "first" : "second") << endl;
         }
         else{
             cout << "another one: ";
         }
     }
-    cout<< "yes";
+    cout << "#PLAYER CHOSEN" << endl;
 
-    bool player;
-    player=(role==1 ? true:false); 
+    bool player = (role == 1 ? true : false); 
     
     //start
 
     //corespondance alphabet - number
     unordered_map<char, int> correspondance;
-    for(int i=0;i<N;i++){
+    for(int i = 0 ; i < N ; i++){
         correspondance.insert({ALPHABET[i],i});
     }
 
-    bool finish=true;
+    cout << "#CORRESPONDANCE" << endl;
+
+    bool finish = true;
+    
+    bool firstRound = true;
     while (finish) {
-        //board.display();
+        board.display();
         if(player){
+            cout << "#PLAYER'S TURN" << endl;
             char l;
-            int x=-1;
-            int y=-1;
-            while(x>15 && x<0 && y>15 && y<0 && board.checkEmpty(x, y)){
-                cout << "Coordonate of the desire move\n x: ";
+            int x = -1;
+            int y = -1;
+            while (!board.checkEmpty(x,y)) {
+                cout << "Coordinate of the desire move\nLine (A to O) : ";
                 cin >> l;
-                if(correspondance.find(l)!=correspondance.end()){
-                    x=correspondance[l];
+                if(correspondance.find(l) != correspondance.end()){
+                    x = correspondance[l];
                 }
-                cout << "y: ";
+                cout << "Column (1 to 15): ";
                 cin >> y;
-                player=(player ? false :true);
             }
-            if(board.winMove(x, y)){
-                cout << "Great Great you win against the amazing me\nCome try again";
-                finish=false;
+            board.setValue(x, y-1, PLAYER_SYMBOL);
+            if(board.winMove(x, y-1)){
+                cout << "Great Great you won against the amazing me\nCome try again";
+                finish = false;
             }
         }
         else{
+            cout << "COMPUTER'S TURN" << endl;
             vector<int> nextMove = brain.nextMove(board);
-            int lig=nextMove[0];
-            int col=nextMove[1];
-            if(board.checkEmpty(lig, col)){
-                if(board.winMove(lig, col)){
-                    cout << "sorry computer better than you AHAHAHA\nCome try again noob"<< endl;
-                    finish=false;
-                }
+            int lig = nextMove[0];
+            int col = nextMove[1];
+            if (board.checkEmpty(lig,col))
+            {
                 board.setValue(lig, col, COMPUTER_SYMBOL);
+                if(board.winMove(lig, col)){
+                    cout << "sorry computer better than you AHAHAHA\nCome try again noob" << endl;
+                    finish = false;
+                }
+            } else {
+                cout << "ERROR : decided to place in an not empty place" << endl;
             }
-            player=(player ? false :true);
-            cout<< "else";
+           
         }
+        player = !player;
     }
 }
 
