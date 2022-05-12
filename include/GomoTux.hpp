70ef3569c31cdb944c394ec61f0367fda3559faa
Iman Alexandre TOURRES <contact@nami10.fr>
2022-05-12 13:56:03 +0200

    adding patterns

---
+++ include/GomoTux.hpp
@@ -0,0 +1,16 @@
+#ifndef GOMOTUX_H
+#define GOMOTUX_H
+
+#include "patterns.hpp"
+
+class GomoTux {
+    private:
+        char **patterns;
+        int *scores;
+
+    public:
+        GomoTux();
+        int evaluation(const char *board);
+}
+
+#endif
---
+++ include/patterns.hpp
@@ -0,0 +1,6 @@
+#ifndef PATTERNS_H
+#define PATTERNS_H
+
+extern char g_patterns[11 * 2][4] ;
+
+#endif
